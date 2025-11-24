f328601e6675069dc484fc2f06e28cfd6ea6b657
Rathi2005 <h0rshitr0thi2005@gmail.com>
2025-11-24 16:29:14 +0530

    ClassWork

---
+++ ClassTask/Intro.js
@@ -0,0 +1,11 @@
+let a = 60;
+let b = 40.2;
+let c = "Harshit";
+let d = {"Harshit" : "Rathi", "Age" : 21};
+let e = [1, 2, 3, 4, 5];
+
+let arr = [a,b,c,d,e];
+
+for(let i=0; i<5; i++){
+    console.log(i + "  :  " + typeof(arr[i]));
+}
